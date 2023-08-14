9ef0f9ead6c3076a714b16d2f054717e799960df
GustavoSilvalgs <luisgust06@gmail.com>
2023-08-14 11:15:56 -0300

    Projeto criado

---
+++ src/test/java/com/squad2/Locadoraveiculos/LocadoraVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad2.Locadoraveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
