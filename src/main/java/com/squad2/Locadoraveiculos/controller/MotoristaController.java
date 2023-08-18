bf3d2198e6b2356563650409157d3fc636cc1234
GustavoSilvalgs <luisgust06@gmail.com>
2023-08-18 14:05:50 -0300

    Customizando a Documentação do Swagger

--- src/main/java/com/squad2/Locadoraveiculos/controller/MotoristaController.java
+++ src/main/java/com/squad2/Locadoraveiculos/controller/MotoristaController.java
@@ -1,9 +1,16 @@
 package com.squad2.Locadoraveiculos.controller;
 
 import com.squad2.Locadoraveiculos.dto.CriarMotoristaDto;
+import com.squad2.Locadoraveiculos.dto.LerMotoristaDto;
 import com.squad2.Locadoraveiculos.models.Motorista;
 import com.squad2.Locadoraveiculos.repositories.MotoristaRepository;
 import com.squad2.Locadoraveiculos.services.MotoristaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/motoristas")
+@Tag(name = "Driver", description = "Endpoints for Managing Drivers")
 public class MotoristaController {
 
     @Autowired
@@ -21,31 +29,96 @@ public class MotoristaController {
 
     @GetMapping(
             produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    public ResponseEntity<?> getAll() {
+    @Operation(summary = "Finds all Drivers", description = "Finds all Drivers",
+            tags = {"Driver"},
+            responses = {
+                @ApiResponse(description = "Success", responseCode = "200",
+                        content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = LerMotoristaDto.class))
+                            )
+                        }),
+                @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
+    public ResponseEntity<?> findAll() {
         return service.listarMotoristas();
     }
 
     @GetMapping(value = "/{id}",
             produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    public ResponseEntity<?> getPessoaById (@PathVariable(value = "id")Long id) {
+    @Operation(summary = "Finds a Driver", description = "Finds a Driver",
+            tags = {"Driver"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = LerMotoristaDto.class))
+                    ),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
+    public ResponseEntity<?> findMotoristaById (@PathVariable(value = "id")Long id) {
         return service.buscarMotoristaPorId(id);
     }
 
     @PostMapping(
             consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
             produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    public ResponseEntity<Motorista> salvar(@RequestBody CriarMotoristaDto motoristaDto) {
+    @Operation(summary = "Adds a new Driver",
+            description = "Adds a new Driver by passing in a JSON or XML representation of the driver!",
+            tags = {"Driver"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = CriarMotoristaDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
+    public ResponseEntity<Motorista> create(@RequestBody CriarMotoristaDto motoristaDto) {
         return service.criarMotorista(motoristaDto);
     }
 
     @PutMapping(
             consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
             produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @Operation(summary = "Updates a Driver",
+            description = "Updates a Driver by passing in a JSON or XML representation of the driver!",
+            tags = {"Driver"},
+            responses = {
+                    @ApiResponse(description = "Updated", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = CriarMotoristaDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
     public ResponseEntity<Motorista> atualizar(@PathVariable Long id, @RequestBody CriarMotoristaDto motorista) {
         return service.atualizarMotorista(id, motorista);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletes a Driver",
+            description = "Deletes a Driver by passing in a JSON or XML representation of the driver!",
+            tags = {"Driver"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         return service.delete(id);
     }
