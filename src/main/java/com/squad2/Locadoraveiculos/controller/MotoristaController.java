dc06a039bba4c9f665d00fafafff170cbb8c7af3
GustavoSilvalgs <luisgust06@gmail.com>
2023-08-18 00:15:59 -0300

    atualizando controller e service

--- src/main/java/com/squad2/Locadoraveiculos/controller/MotoristaController.java
+++ src/main/java/com/squad2/Locadoraveiculos/controller/MotoristaController.java
@@ -5,12 +5,8 @@ import com.squad2.Locadoraveiculos.models.Motorista;
 import com.squad2.Locadoraveiculos.repositories.MotoristaRepository;
 import com.squad2.Locadoraveiculos.services.MotoristaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/motoristas")
@@ -42,31 +38,8 @@ public class MotoristaController {
         return service.listarMotoristas();
     }
 
-    /*@GetMapping("{id}")
-    public Motorista getPessoaById (@PathVariable Long id) {
-
-        return motoristaRepository.findById(id).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getPessoaById (@PathVariable(value = "id")Long id) {
+        return service.buscarMotoristaPorId(id);
     }
-
-    @DeleteMapping("{id}")
-    public void deletar (@PathVariable Long id) {
-        motoristaRepository.findById(id).map( motorista -> {
-                motoristaRepository.delete(motorista);
-                return Void.TYPE;})
-                    .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    }*/
-
-    /*@PutMapping("{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void atualizar(@PathVariable Long id, @RequestBody Motorista motorista){
-        motoristaRepository.findById(id).map(motorista1 -> {
-            motorista.setId(motorista1.getId());
-            return motoristaRepository.save(motorista);
-        }).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-    }*/
-
-
 }
