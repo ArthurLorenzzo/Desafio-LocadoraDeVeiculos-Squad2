2f06f199b4c948e29c47df5f9c15872d7754f735
IsabelleCarvallho <carvalhoisabelle74@gmail.com>
2023-08-21 17:24:42 -0300

    Refactor: service e controller de funcionario

--- src/main/java/com/squad2/Locadoraveiculos/controllers/FuncionarioController.java
+++ src/main/java/com/squad2/Locadoraveiculos/controllers/FuncionarioController.java
@@ -4,6 +4,7 @@ import com.squad2.Locadoraveiculos.dtos.FuncionarioDto;
 import com.squad2.Locadoraveiculos.dtos.MotoristaDto;
 import com.squad2.Locadoraveiculos.services.FuncionarioService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -12,6 +13,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/funcionarios")
@@ -23,9 +26,9 @@ public class FuncionarioController {
     @PostMapping(
             consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
             produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    @Operation(summary = "Adds a new Driver",
-            description = "Adds a new Driver by passing in a JSON or XML representation of the driver!",
-            tags = {"Driver"},
+    @Operation(summary = "Adicionando um novo funcionário",
+            description = "Adiciona um novo funcionário passando uma representação JSON ou XML !",
+            tags = {"funcionário"},
             responses = {
                     @ApiResponse(description = "Success", responseCode = "200",
                             content = @Content(schema = @Schema(implementation = FuncionarioDto.class))
@@ -41,8 +44,61 @@ public class FuncionarioController {
     }
 
 
-
-
-
+    @Operation(summary = "Procura um funcionário por ID", description = "Procura um funcionário por ID",
+            tags = {"funcionário"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = MotoristaDto.class))
+                    ),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
+    @GetMapping(value = "/{id}",
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    public FuncionarioDto findById (@PathVariable(value = "id") Long id) {
+        return funcionarioService.findById(id);
+    }
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Deleta um Funcionário",
+            description = "Exclui um funcionário passando uma representação JSON ou XML!!",
+            tags = {"funcionário"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+        funcionarioService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+    @GetMapping(
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @Operation(summary = "Procurando todos os funcionários", description = "Procurando todos os funcionários",
+            tags = {"funcionário"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = MotoristaDto.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
+    public ResponseEntity<List<FuncionarioDto>> findAll() {
+        List<FuncionarioDto> listaFuncionarioDto = funcionarioService.findAll();
+        return ResponseEntity.ok(listaFuncionarioDto);
+    }
 
 }
--- src/main/java/com/squad2/Locadoraveiculos/services/FuncionarioService.java
+++ src/main/java/com/squad2/Locadoraveiculos/services/FuncionarioService.java
@@ -1,14 +1,23 @@
 package com.squad2.Locadoraveiculos.services;
 
 import com.squad2.Locadoraveiculos.dtos.FuncionarioDto;
+import com.squad2.Locadoraveiculos.dtos.MotoristaDto;
+import com.squad2.Locadoraveiculos.exceptions.ResourceNotFoundException;
 import com.squad2.Locadoraveiculos.models.Funcionario;
+import com.squad2.Locadoraveiculos.models.Motorista;
 import com.squad2.Locadoraveiculos.repositories.FuncionarioRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
 @Service
 public class FuncionarioService {
+
+    private Logger logger = Logger.getLogger(MotoristaService.class.getName());
 
     @Autowired
     private FuncionarioRepository funcionarioRepository;
@@ -20,11 +29,34 @@ public class FuncionarioService {
         BeanUtils.copyProperties(funcionario,funcionarioDto);
         return funcionarioDto;
     }
+    public FuncionarioDto findById(Long id) {
+        logger.info("Procurando um funcionário");
 
+        Funcionario funcionario = funcionarioRepository
+                .findById(id)
+                .orElseThrow(ResourceNotFoundException::new);
+        FuncionarioDto FuncionarioDto = new FuncionarioDto();
+        BeanUtils.copyProperties(funcionario, FuncionarioDto);
 
+        return FuncionarioDto;
+    }
+    public void delete(Long id) {
+        logger.info("Deletando funcionário");
 
+        var entity = funcionarioRepository.findById(id)
+                .orElseThrow(ResourceNotFoundException::new);
+        funcionarioRepository.delete(entity);
+    }
 
-
-
-
+    public List<FuncionarioDto> findAll() {
+        logger.info("Procurando todos os funcionários");
+        List<Funcionario> listaFuncionario = funcionarioRepository.findAll();
+        return listaFuncionario.stream()
+                .map(funcionario -> {
+                    FuncionarioDto funcionarioDto = new FuncionarioDto();
+                    BeanUtils.copyProperties(funcionario, funcionarioDto);
+                    return funcionarioDto;
+                })
+                .collect(Collectors.toList());
+    }
 }
